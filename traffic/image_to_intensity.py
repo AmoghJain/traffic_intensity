c9679542514951ca4e96aa77ed8065ad30f3fed1
amoghjain <amoghjainsp@gmail.com>
2018-04-19 18:03:55 +0530

    modified images processing code

--- traffic/image_to_intensity.py
+++ traffic/image_to_intensity.py
@@ -1,31 +1,44 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
+import os
 
-img = cv2.imread("scr3.png")
-high = 0
-mid = 0
-low = 0
+def extract_traffic_layer(base_dir):
 
-new_image = []
-for row in img:
-	new_row = []
-	for col in row:
-		if col[2] > 120 and col[1] < 50: # red
-			# high+=1
-			new_row.append(col)
-		elif col[2] < 135 and col[1] > 200: # green 
-			# low+=1
-			new_row.append(col)
-		elif col[2] > 200 and col[1] > 100 and col[0] < 20: # orange
-			# mid+=1
-			new_row.append(col)
-		else:
-			new_row.append(np.array([0,0,0]))
-	new_image.append(new_row)
+	output_directory = base_dir[:-1]+"_processed/"
+	if not os.path.exists(output_directory):
+		os.makedirs(output_directory)
 
-new_image = np.array(new_image)
-cv2.imwrite("processed3.png", new_image)
+	for file_ in os.listdir(base_dir):
+		print(file_)
+		img = cv2.imread(base_dir+file_)
+		# high = 0
+		# mid = 0
+		# low = 0
+
+		new_image = []
+		for row in img:
+			new_row = []
+			for col in row:
+				if col[2] > 120 and col[1] < 50: # red
+					# high+=1
+					new_row.append(col)
+				elif col[2] < 135 and col[1] > 200: # green 
+					# low+=1
+					new_row.append(col)
+				elif col[2] > 200 and col[1] > 100 and col[0] < 20: # orange
+					# mid+=1
+					new_row.append(col)
+				else:
+					new_row.append(np.array([0,0,0]))
+			new_image.append(new_row)
+
+		new_image = np.array(new_image)
+		cv2.imwrite(output_directory+file_, new_image)
+		# break
+
+extract_traffic_layer(sys.argv[1])
 
 # total = high+mid+low
 
--- traffic/screenshot.py
+++ traffic/screenshot.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 
-browser = webdriver.PhantomJS()
+browser = webdriver.Firefox()
 browser.get('http://localhost:7001/')
 browser.save_screenshot("images/"+"_".join(str(time.time()).split("."))+'.png')
 browser.quit()
